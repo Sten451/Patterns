252e44cad954392fce3d5d7763c4f60dc1105665
Sten <lvv_1@list.tu>
2022-03-28 22:52:31 +0300

    dz_1

---
+++ run.py
@@ -0,0 +1,9 @@
+from light_framework.main import Framework
+from urls import routes, fronts
+from wsgiref.simple_server import make_server
+
+application = Framework(routes, fronts)
+
+with make_server('', 8000, application) as httpd:
+    print(f"Запуск на порту 8000...")
+    httpd.serve_forever()
