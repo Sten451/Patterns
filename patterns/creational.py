cac6e7c684cbae98aab456226e9f524d28e35f53
Sten <lvv_1@list.tu>
2022-04-16 01:08:26 +0300

    dz_4

--- patterns/creational.py
+++ patterns/creational.py
@@ -1,5 +1,7 @@
-
-
+import copy
+import ast
+from datetime import datetime
+import quopri
 
 class User:
     pass
@@ -10,42 +12,60 @@ class Admin(User):
 
 # клиент
 class Client(User):
-    pass
+    def __init__(self):
+        self.active_client = ''
+        self.clients = [{"login": "sten", "password": "123"},{"login": "mad", "password": "123"}]
+    
+
+    def login_client(self,  list_clients, login, password):
+        for client in list_clients:
+            if login == client['login'] and password == client['password']:
+                return True
+        return False    
+
+class Product:
+    def __init__(self):
+        self.flowers = {'Роза': 100, 'Лилия': 500, 'Тюльпан': 50, 'Хризонтема': 70, 'Составление букета': 500}
+        self.airs = {'Плюшевый мишка': 500, 'Набор воздушных шаров': 700, 'Собачка': 300, 'Кошечка': 200}
+        self.cakes = {'Раффаэлло': 500, 'Ферреро роше': 700, 'Мерси ассорти': 300, 'Коркунов': 200}
+
+
+class CartCreational:
+    def __init__(self):
+        self.cart = []
+        self.status = "У Вас нет неотправленных заказов"
+
+    def clone(self, dict):
+        new_list = dict['copy_param']
+        arr = ast.literal_eval(new_list)
+        return copy.deepcopy(arr)
+
+    def append_product(self, old_cart, new_data):
+        return old_cart.append(new_data)
+
+    def extend_cart(self, old_cart, new_data):
+        return old_cart.extend(new_data)
+
+    def clear_cart(self, data):
+        return data.clear()
 
 
 class Engine:
     def __init__(self):
-        self.clients = [{"login": "sten", "password": "123"},{"login": "mad", "password": "123"}]
-        self.active_user = ''
-        self.flowers = {'Роза': 100, 'Лилия': 500, 'Тюльпан': 50, 'Хризонтема': 70, 'Составление букета': 500}
-        self.airs = {'Плюшевый мишка': 500, 'Набор воздушных шаров': 700, 'Собачка': 300, 'Кошечка': 200}
-        self.cakes = {'Раффаэлло': 500, 'Ферреро роше': 700, 'Мерси ассорти': 300, 'Коркунов': 200}
-        self.cart = []
-        self.status = "У Вас нет неотправленных заказов"
-
+        pass
+        
     @staticmethod
-    def create_user(type_):
-        return UserFactory.create(type_)
-
-
-
-class UserFactory:
-    types = {
-        'administrator': Admin,
-        'client': Client
-    }
-
-    # порождающий паттерн Фабричный метод
-    @classmethod
-    def create(cls, type_):
-        return cls.types[type_]()
-
-
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
 
 
 # порождающий паттерн Синглтон
 class SingletonByName(type):
-
     def __init__(cls, name, bases, attrs, **kwargs):
         super().__init__(name, bases, attrs)
         cls.__instance = {}
@@ -70,3 +90,9 @@ class Logger(metaclass=SingletonByName):
     @staticmethod
     def log(text):
         print('log--->', text)
+        with open('admin/log.txt', 'a', encoding='utf-8') as f:
+            current_date = str(datetime.now())
+            f.write(current_date + ' ' + text + '\n')
+            
+
+
--- views.py
+++ views.py
@@ -1,137 +1,132 @@
 import json
 from datetime import datetime
-import quopri
-from threading import active_count
 from light_framework.templator import render
-from patterns.creational import Engine, Logger
+from patterns.creational import CartCreational, Engine, Logger, Product, Client
 
 
+#экземпляры классов
 site = Engine()
 logger = Logger('main')
+mycart = CartCreational()
+product = Product()
+client_list = Client()
 
 
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html', active_user = site.active_user)
+        return '200 OK', render('index.html', active_user = client_list.active_client)
+
 
 class Flowers:
     def __call__(self, request):
-        return '200 OK', render('flowers.html', flowers = site.flowers, active_user = site.active_user)
-        
+        return '200 OK', render('flowers.html', flowers = product.flowers, active_user = client_list.active_client)
+
+
 class Air:
     def __call__(self, request):
-        return '200 OK', render('airs.html', airs = site.airs, active_user = site.active_user)
+        return '200 OK', render('airs.html', airs = product.airs, active_user = client_list.active_client)
+
 
 class Cake:
     def __call__(self, request):
-        return '200 OK', render('cakes.html', cakes=site.cakes, active_user = site.active_user)
+        return '200 OK', render('cakes.html', cakes = product.cakes, active_user = client_list.active_client)
+
 
 class Meeting:
     def __call__(self, request):
-        return '200 OK', render('meeting.html', active_user = site.active_user)
+        return '200 OK', render('meeting.html', active_user = client_list.active_client)
+
 
 class LK:
     def __call__(self, request):
-        return '200 OK', render('lk.html', active_user = site.active_user, cart = site.cart, status = site.status)
+        return '200 OK', render('lk.html', active_user = client_list.active_client, cart = mycart.cart, status = mycart.status)
 
 
 class Logout:
     def __call__(self, request):
-        site.active_user = ""
-        site.status = "У Вас нет неотправленных заказов"
-        site.cart.clear()
-        return '200 OK', render('index.html', active_user = site.active_user)
+        logger.log(f"{client_list.active_client} разлогинился.")
+        client_list.active_client = ""
+        mycart.status = "У Вас нет неотправленных заказов"
+        # Очищаем корзину
+        mycart.clear_cart(mycart.cart)
+        return '200 OK', render('index.html', active_user = client_list.active_client)
+
 
 class Contact:
     def __call__(self, request):
         if request['method'] == 'POST':
-            with open('question.txt', 'a', encoding="utf-8") as f:
-                dict = Contact.decode_value(request['data'])
+            with open('admin/question.txt', 'a', encoding="utf-8") as f:
+                dict = site.decode_value(request['data'])
                 str_dict = json.dumps(dict, ensure_ascii=False)
                 current_date = str(datetime.now())
-                f.write(current_date + str_dict + '\n')
-                logger.log(f"Сообщение успешно записано в файл.")
-        return '200 OK', render('contact.html', active_user = site.active_user)
+                f.write(current_date + ' ' + str_dict + '\n')
+                logger.log(f"{client_list.active_client} направил сообщение в службу поддержки.")
+        return '200 OK', render('contact.html', active_user = client_list.active_client)
     
-    @staticmethod
-    def decode_value(data):
-        new_data = {}
-        for k, v in data.items():
-            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
-            val_decode_str = quopri.decodestring(val).decode('UTF-8')
-            new_data[k] = val_decode_str
-        return new_data
-
 
 class Login:
     def __call__(self, request):
         if request['method'] == 'POST':
-            for client in site.clients:
-                if request['data']['login'] == client['login'] and request['data']['password'] == client['password']:
-                    logger.log(f"Пользователь {request['data']['login']} вошел в систему")
-                    site.active_user = client['login']
-                    return '200 OK', render('index.html', active_user = site.active_user)
-
+            # Функция аутентификации передаем список клиентов, логин, пароль    
+            if client_list.login_client(client_list.clients, request['data']['login'], request['data']['password']):
+                logger.log(f"Пользователь {request['data']['login']} вошел в систему")
+                client_list.active_client = request['data']['login']
+                return '200 OK', render('index.html', active_user = client_list.active_client)
         return '200 OK', render('login.html')
+
 
 class Registration:
     def __call__(self, request):
         if request['method'] == 'POST':
             if request['data']['password'] != request['data']['password2']:
-                logger.log('Пароли не совпадают.')
+                logger.log(f"Для пользователя {request['data']['login']} Пароли при регистрации не совпали.")
                 return '200 OK', render('reg.html')    
-            for client in site.clients:
+            for client in client_list.clients:
                 if request['data']['login'] == client['login']:
-                    logger.log('Такой Пользователь уже существует')
+                    logger.log(f"Пользователь {request['data']['login']} уже зарегистрирован на сайте.")
                     return '200 OK', render('reg.html')
-            site.clients.append({"login":request['data']['login'], "password":request['data']['password']})    
+            client_list.clients.append({"login":request['data']['login'], "password":request['data']['password']})    
             logger.log(f"Пользователь {request['data']['login']} успешно зарегистрирован.")
-            site.active_user = request['data']['login']
-            return '200 OK', render('index.html', active_user = site.active_user)
-         
+            client_list.active_client = request['data']['login']
+            return '200 OK', render('index.html', active_user = client_list.active_client)
         return '200 OK', render('reg.html')
+
 
 class NotFound404:
     def __call__(self, request):
         return '404 NOT FOUND', render('404.html')
 
+
 class Cart:
     def __call__(self, request):
-        dict = Cart.decode_value(request['request_params'])
-        site.cart.append(dict)
-        logger.log(f"Товар {dict} добавлен в корзину")
-        return '200 OK', render('lk.html', active_user = site.active_user, cart = site.cart)
-
-    @staticmethod
-    def decode_value(data):
-        new_data = {}
-        for k, v in data.items():
-            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
-            val_decode_str = quopri.decodestring(val).decode('UTF-8')
-            new_data[k] = val_decode_str
-        return new_data
+        dict = site.decode_value(request['request_params'])
+        # вызывааем функцию добавления товара
+        mycart.append_product(mycart.cart, dict)
+        logger.log(f"{client_list.active_client} добавил товар {dict} в корзину")
+        return '200 OK', render('lk.html', active_user = client_list.active_client, cart = mycart.cart)
 
 
 class Order:
     def __call__(self, request):
         if request['method'] == 'POST':
-            with open('order.txt', 'a', encoding="utf-8") as f:
-                # получаем словарь
-                dict = Order.decode_value(request['data'])
+            with open('admin/order.txt', 'a', encoding="utf-8") as f:
+                dict = site.decode_value(request['data'])
                 str_dict = json.dumps(dict, ensure_ascii=False)
                 current_date = str(datetime.now())
-                f.write(current_date + str_dict + '\n')
-                logger.log(f"Заказ успешно записан в файл.")
-                site.cart.clear()
-                site.status = "Заказ отправлен на оформление, ожидайте менеджер свяжется с Вами."
-        return '200 OK', render('lk.html', active_user = site.active_user, cart = site.cart, status = site.status)
+                f.write(current_date + ' ' + str_dict + '\n')
+                logger.log(f"{client_list.active_client} отправил заказ на оформление.")
+                mycart.clear_cart(mycart.cart)
+                mycart.status = "Заказ отправлен на оформление, ожидайте менеджер свяжется с Вами."
+        return '200 OK', render('lk.html', active_user = client_list.active_client, cart = mycart.cart, status = mycart.status)
     
-    @staticmethod
-    def decode_value(data):
-        new_data = {}
-        for k, v in data.items():
-            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
-            val_decode_str = quopri.decodestring(val).decode('UTF-8')
-            new_data[k] = val_decode_str
-        return new_data
-        
+ 
+class CopyCart:
+    def __call__(self, request):
+        if request['method'] == 'POST':
+            dict = site.decode_value(request['data'])
+            new_cart = mycart.clone(dict)
+            logger.log(f"{client_list.active_client} успешно продублировал заказ.")
+            # Вызывем функцию клонирования путём слияния массивов            
+            mycart.extend_cart(mycart.cart, new_cart)
+        return '200 OK', render('lk.html', active_user = client_list.active_client, cart = mycart.cart, status = mycart.status)
+    
