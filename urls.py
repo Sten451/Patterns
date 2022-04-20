3b14fb7c8a21f4fa3ea6f9e251960f2358dab131
Sten <lvv_1@list.tu>
2022-04-20 13:42:27 +0300

    dz_5

--- urls.py
+++ urls.py
@@ -1,5 +1,3 @@
-from views import Index, Flowers, Cake, Login, Meeting, Air, Contact, Registration, Login, LK, Logout, Cart, Order, CopyCart
-
 
 # front controller
 def secret_front(request):
@@ -12,22 +10,3 @@ def other_front(request):
 
 fronts = [secret_front, other_front]
 
-
-routes = {
-    '/': Index(),
-    '/flowers/': Flowers(),
-    '/air/': Air(),
-    '/cake/': Cake(),
-    '/meeting/': Meeting(),
-    '/contact/': Contact(),
-    '/registration/': Registration(),
-    '/login/': Login(),
-    '/cabinet/': LK(),
-    '/logout/': Logout(),
-    '/cart/': Cart(),
-    '/order/': Order(),
-    '/copy_cart/': CopyCart(),
-
-
-
-}
